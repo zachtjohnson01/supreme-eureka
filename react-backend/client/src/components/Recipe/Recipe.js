add24bc2b1e055236007061cf055c608b1571593
Zach Johnson <zachtjohnson01@gmail.com>
2018-09-22 12:03:14 -0500

    Added recipe parsing input and display

--- react-backend/client/src/components/Recipe/Recipe.js
+++ react-backend/client/src/components/Recipe/Recipe.js
@@ -4,13 +4,14 @@ import "../../App.css";
 import "./Recipe.css";
 import axios from "axios";
 import fs from "fs";
+import update from 'immutability-helper';
 // import { parse } from "recipe-ingredient-parser";
 
 class Recipe extends Component {
     constructor(props) {
         super();
         this.state = {
-            recipe: 'ingredient'
+            // recipe: 'ingredient'
         }
     }
 
@@ -25,29 +26,46 @@ class Recipe extends Component {
 
     parseRecipe = () => {
         let ingredient = this.state.recipe;
-        let ingArray;
-        ingArray = ingredient.split("\n").join("||");
+        let ingrArray;
+        ingrArray = ingredient.split("\n").join("||");
 
         axios
-            .get("api/recipeParser/" + ingArray)
+            .get("api/recipeParser/" + ingrArray)
             .then(response => {
-                console.log(response)
-                // this.setState({
-                //     parsed: response
-                // })
+                const resp = response.data;
+                const parsedRecipe = update(this.state.parsedRecipe, {
+                     $set: resp }
+                )
+                console.log(response.data)
+                this.setState({
+                    parsedRecipe
+                })
             })
     }
 
     render() {
+
+        if (this.state.parsedRecipe) {
+            var parsedIngredientes = 
+                [...this.state.parsedRecipe].map(recipe => {
+                    return (
+                        <div className="recipe-ingredient">
+                            <div className="recipe-ingredient-item"> {recipe.quantity} </div>
+                            <div className="recipe-ingredient-item"> {recipe.unit} </div>
+                            <div className="recipe-ingredient-item"> {recipe.ingredient} </div>
+                        </div>
+                    )
+                })
+        }
         return(
             <div className="container">
                 <div>Recipe</div>
                 <div className="recipeContainer">
                     <textarea className="recipe-item recipe-input" type="text" onChange={this.handleChange.bind(this)}/>
                     <div className="recipe-item recipe-output">
-                        ({this.state.recipe.split('\r\n')})
-                        {this.state.parsed}
+                        {/* ({this.state.recipe.split('\r\n')}) */}
                         <button className="recipe-parse" onClick={this.parseRecipe.bind(this)}>Parse Recipe</button>
+                        { parsedIngredientes}
                     </div>
                 </div>
             </div>
